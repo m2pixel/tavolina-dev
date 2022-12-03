cd014469a4a9e0c957c5da5e795fda229a380eae
m2pixel <m2pixell@gmail.com>
2022-12-03 18:12:59 +0100

    server3

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,7 @@ app.use('/api/stock', require('./routes/stockRoutes'))
 
 // Serve frontend
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../frontend/build')))
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
 
   app.get('*', (req, res) =>
     res.sendFile(
