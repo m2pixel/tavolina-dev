0aa842a13809e20bdc7ea02dc3c219d81faefec8
m2pixel <m2pixell@gmail.com>
2022-12-03 17:05:27 +0100

    server

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const colors = require('colors')
 const dotenv = require('dotenv').config()
 const { errorHandler } = require('./middleware/errorMiddleware')
 const connectDB = require('./config/db')
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 3000
 const path = require('path')
 connectDB()
 
@@ -25,11 +25,11 @@ app.use('/api/stock', require('./routes/stockRoutes'))
 
 // Serve frontend
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../frontend/build')))
+  app.use(express.static(path.join(__dirname, './frontend/build')))
 
   app.get('*', (req, res) =>
     res.sendFile(
-      path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
+      path.resolve(__dirname, './', 'frontend', 'build', 'index.html')
     )
   )
 } else {
