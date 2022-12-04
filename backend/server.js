dec4e292ca7763d077c39473c27b5ed388e1ef52
m2pixel <m2pixell@gmail.com>
2022-12-04 21:22:09 +0100

    cors advanced

--- backend/server.js
+++ backend/server.js
@@ -17,8 +17,22 @@ app.use(
   cors({
     origin: 'https://tavolina-front.onrender.com',
     credentials: true,
+    // Set the browser cache time for preflight responses
+    maxAge: 86400,
+    preflightContinue: true, // Allow us to manually add to preflights
   })
 )
+
+// Add cache-control to preflight responses in a separate middleware:
+app.use((req, res, next) => {
+  if (req.method === 'OPTIONS') {
+    res.setHeader('Cache-Control', 'public, max-age=86400')
+    // No Vary required: cors sets it already set automatically
+    res.end()
+  } else {
+    next()
+  }
+})
 
 app.use('/api/categories', require('./routes/categoryRoutes'))
 app.use('/api/users', require('./routes/userRoutes'))
